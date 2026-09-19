fa02a81881d4bae902edbf28658578bcfac27f81
first-uninteresting-username <196055729+first-uninteresting-username@users.noreply.github.com>
2026-09-19 21:24:49 +0000

    style: nix maintenance (statix, deadnix, alejandra)

--- modules/configuration/applications/gaming.nix
+++ modules/configuration/applications/gaming.nix
@@ -1,10 +1,7 @@
 # SPDX-FileCopyrightText: 2026 first-uninteresting-username
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-{
-  inputs,
-  ...
-}: {
+{inputs, ...}: {
   flake = {
     nixosModules = {
       gaming-distrobox = {
